8070c8eee9529c549e7bb68f16e51edf2ddf0051
David F. Severski <davidski@deadheaven.com>
2016-12-10 08:40:24 -0800

    (Hopefully) temporary hack build process until apex build zips can be fixed for Windows

---
+++ build-artifacts.ps1
@@ -0,0 +1,6 @@
+﻿$paths = Get-ChildItem ./functions
+foreach ($item in $paths) {
+    $dest = ($item -replace "-", "_") + ".zip"
+    Write-Output "Destination is $dest"
+    Compress-Archive -Path functions/$item/main.py -DestinationPath E:/terraform/personal-infrastructure/files/$dest -force -Verbose
+} 
